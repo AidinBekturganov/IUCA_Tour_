23e8f5737e8989e229d3287df4bb148eb2b1cfae
Aidin <bekturganovaidin@gmail.com>
2021-12-29 21:54:23 +0600

    Initial Commit

---
+++ IUCA Tour/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IUCA Tour
+//
+//  Created by User on 12/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
